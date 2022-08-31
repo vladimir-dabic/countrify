1e042c9b86902112c77d9555a910f08543a22d80
Vladimir <vladimir@solrise.finance>
2022-08-31 21:55:52 +0200

    filter coutries

--- src/store/actions/gameAction.js
+++ src/store/actions/gameAction.js
@@ -1,42 +1,42 @@
-import api from '../../services/countries'
-import { newQuestions } from './flagVSCountry.action'
+import api from "../../services/countries";
+import { newQuestions } from "./flagVSCountry.action";
 
 export const handleGame = (payload) => {
   return {
-    type: 'CHOOSE_GAME',
-    payload
-  }
-}
+    type: "CHOOSE_GAME",
+    payload,
+  };
+};
 
 export const exitGame = () => {
-  return { type: 'EXIT_GAME'}
-}
-
+  return { type: "EXIT_GAME" };
+};
 
 export const getCountries = (region, numberOfCountries, numberOfRounds) => {
-
   return async (dispatch, getState) => {
     try {
       dispatch({
-        type: 'PRE_GET_COUNTRIES',
-        payload: {loading: true}
-      })
+        type: "PRE_GET_COUNTRIES",
+        payload: { loading: true },
+      });
 
-      let response = await api.get(`region/${region}`)
+      let result = await api.get(`region/${region}`);
+      let response = result.data.filter((c) => c.name !== "Republic of Kosovo");
+      console.log({ response });
 
       dispatch({
-        type: 'GET_COUNTRIES',
-        payload: {data: response.data, loading: true}
-      })
+        type: "GET_COUNTRIES",
+        payload: { data: response.data, loading: true },
+      });
 
-      dispatch(newQuestions(null, response.data, numberOfCountries, numberOfRounds))
-
+      dispatch(
+        newQuestions(null, response.data, numberOfCountries, numberOfRounds)
+      );
     } catch (error) {
       dispatch({
-        type: 'ERR_GET_COUNTRIES',
-        payload: {loading: false, error}
-      })
+        type: "ERR_GET_COUNTRIES",
+        payload: { loading: false, error },
+      });
     }
-
-  }
-}
+  };
+};
